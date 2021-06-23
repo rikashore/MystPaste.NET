9d86a119a899488af292b76b2ae363288d771abd
Aarav Navani <aarav.navani@outlook.com>
2021-06-23 21:14:36 +0530

    Added StringExtensions

---
+++ MystPaste.NET/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace MystPaste.NET.Extensions
+{
+    public static class StringExtensions
+    {
+        public static Uri FormatUri(this string pattern, params object[] args)
+        {
+            if (string.IsNullOrWhiteSpace(pattern))
+                throw new ArgumentNullException(nameof(pattern));
+
+            return new Uri(string.Format(CultureInfo.InvariantCulture, pattern, args), UriKind.Relative);
+        }
+    }
+}
