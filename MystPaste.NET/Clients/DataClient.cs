41601fc546c268b6297f70f67165ecaff03b1d58
Aarav Navani <aarav.navani@outlook.com>
2021-06-23 21:13:53 +0530

    Added GetLanguageByExtensionAsync

--- MystPaste.NET/Clients/DataClient.cs
+++ MystPaste.NET/Clients/DataClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Threading.Tasks;
+using MystPaste.NET.Helpers;
 using MystPaste.NET.Models;
 
 namespace MystPaste.NET.Clients
@@ -13,7 +14,13 @@ namespace MystPaste.NET.Clients
         public async Task<Language> GetLanguageByNameAsync(string languageName)
         {
             var encodedName = WebUtility.UrlEncode(languageName);
-            return await ApiRequester.Get<Language>(new Uri($"{BaseUri}/data/language?name={encodedName}"));
+            return await ApiRequester.Get<Language>(ApiUrls.LanguageByName(encodedName));
+        }
+
+        public async Task<Language> GetLanguageByExtensionAsync(string extension)
+        {
+            var encodedExtension = WebUtility.UrlEncode(extension);
+            return await ApiRequester.Get<Language>(ApiUrls.LanguageByExtension(encodedExtension));
         }
     }
 }
