15db4d0539e290bed8921dc78555fd3b67e01e5f
Aarav Navani <aarav.navani@outlook.com>
2021-06-23 21:13:40 +0530

    Added API Urls

---
+++ MystPaste.NET/Helpers/ApiUrls.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MystPaste.NET.Extensions;
+
+namespace MystPaste.NET.Helpers
+{
+    public static class ApiUrls
+    {
+        public static Uri LanguageByName(string language)
+            => "data/language?name={0}".FormatUri(language);
+
+        public static Uri LanguageByExtension(string extension)
+            => "/data/languageExt?extension={0}".FormatUri(extension);
+    }
+}
