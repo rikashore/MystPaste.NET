e46fb58968ffdaa67941de8091aa5f36a9b9690c
Aarav Navani <aarav.navani@outlook.com>
2021-06-24 08:25:41 +0530

    Added UserClient

---
+++ MystPaste.NET/Clients/UserClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using MystPaste.NET.Helpers;
+using MystPaste.NET.Models;
+
+namespace MystPaste.NET.Clients
+{
+    // TODO Map endpoints that require auth
+    public class UserClient : ApiClient
+    {
+        public UserClient(ApiRequester apiRequester) : base(apiRequester)
+        { }
+
+        public async Task<bool> CheckIfUserExistsAsync(string username)
+        {
+            return await ApiRequester.Get(ApiUrls.UserExists(username));
+        }
+
+        public async Task<User> GetUserAsync(string username)
+        {
+            return await ApiRequester.Get<User>(ApiUrls.User(username));
+        }
+    }
+}
