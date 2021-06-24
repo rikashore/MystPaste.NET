0f051feb033d67280e6ba70496d00d6606a631a2
Aarav Navani <aarav.navani@outlook.com>
2021-06-24 21:00:52 +0530

    Added InvalidAuthException

---
+++ MystPaste.NET/Helpers/Exceptions/InvalidAuthException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MystPaste.NET.Helpers.Exceptions
+{
+    public class InvalidAuthException : Exception
+    {
+        public InvalidAuthException(string paramName)
+        {
+            Message = "An authorization token needs to be passed in the constructor of the MystPasteClient or to the method";
+            ParamName = paramName;
+        }
+
+        public override string Message { get; }
+        public string ParamName { get; }
+    }
+}
