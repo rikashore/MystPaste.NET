b501745d7a249c3c6daa7eabf51c4953634e8d32
Aarav Navani <aarav.navani@outlook.com>
2021-06-24 14:07:19 +0530

    Documented the Requester

--- MystPaste.NET/ApiRequester.cs
+++ MystPaste.NET/ApiRequester.cs
@@ -5,6 +5,9 @@ using MystPaste.NET.Extensions;
 
 namespace MystPaste.NET
 {
+    /// <summary>
+    /// Represents the class used to make API requests.
+    /// </summary>
     public class ApiRequester
     {
         private readonly HttpClient _httpClient;
@@ -15,6 +18,9 @@ namespace MystPaste.NET
             _httpClient = new HttpClient();
         }
 
+        /// <summary>
+        /// Makes a GET request to the specified url.
+        /// </summary>
         public async Task<T> Get<T>(Uri uri)
         {
             var res = await _httpClient.GetAsync(new Uri(_baseUri, uri));
@@ -25,6 +31,9 @@ namespace MystPaste.NET
             return s.DeserializeTo<T>();
         }
 
+        /// <summary>
+        /// Makes a GET request to the specified url. Used to check if a resource exists.
+        /// </summary>
         public async Task<bool> Get(Uri uri)
         {
             var res = await _httpClient.GetAsync(new Uri(_baseUri, uri));
