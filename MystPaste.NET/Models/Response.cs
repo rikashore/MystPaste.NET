90fbec28aa6d5973d65db8db2a9314bb5995a0b3
Aarav Navani <aarav.navani@outlook.com>
2021-06-24 15:42:28 +0530

    Added response model

---
+++ MystPaste.NET/Models/Response.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace MystPaste.NET.Models
+{
+    /// <summary>
+    /// Represents a model for an API error response.
+    /// </summary>
+    public class Response
+    {
+        /// <summary>
+        /// The status/error message returned by the server.
+        /// </summary>
+        [JsonProperty("statusMessage")]
+        public string ErrorMessage { get; }
+    }
+}
