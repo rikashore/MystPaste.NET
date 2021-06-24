3b75b5b291de1b1ed9bbc115a81b246a1c538be2
Aarav Navani <aarav.navani@outlook.com>
2021-06-24 17:16:42 +0530

    Added EditType

---
+++ MystPaste.NET/Helpers/Pastes/EditType.cs
@@ -0,0 +1,23 @@
+﻿namespace MystPaste.NET.Helpers.Pastes
+{
+    public enum EditType
+    {
+        /// <summary>The paste's title was edited.</summary>
+        PasteTitleEdited = 0,
+
+        /// <summary>A pasty's title was edited.</summary>
+        PastyTitleEdited = 1,
+
+        /// <summary>A pasty's language was updated.</summary>
+        PastyLanguageUpdated = 2,
+
+        /// <summary>A pasty's code content was updated.</summary>
+        PastyContentUpdated = 3,
+
+        /// <summary>A pasty was added into the paste.</summary>
+        PastyAdded = 4,
+
+        /// <summary>A pasty was removed from the paste.</summary>
+        PastyRemoved = 5
+    }
+}
