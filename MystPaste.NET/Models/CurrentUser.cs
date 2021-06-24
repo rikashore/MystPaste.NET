dabec6ca7c33a864f1513ef92b649a2e7d4779ea
Aarav Navani <aarav.navani@outlook.com>
2021-06-24 15:44:44 +0530

    Finished CurrentUser model

--- MystPaste.NET/Models/CurrentUser.cs
+++ MystPaste.NET/Models/CurrentUser.cs
@@ -1,8 +1,23 @@
-﻿namespace MystPaste.NET.Models
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace MystPaste.NET.Models
 {
-    // TODO figure out inheritance based deserialization
-    public class CurrentUser
+    /// <summary>
+    /// Represents the currently logged in user.
+    /// </summary>
+    public class CurrentUser : User
     {
+        /// <summary>
+        /// A list of paste ids the user has starred.
+        /// </summary>
+        [JsonProperty("stars")]
+        public List<string> Stars { get; }
         
+        /// <summary>
+        /// User ids of the service the user used to create an account.
+        /// </summary>
+        [JsonProperty("serviceIds")]
+        public IDictionary<string, string> ServiceIds { get; }
     }
 }
