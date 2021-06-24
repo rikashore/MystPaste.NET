0eeb687e43759206ddf710bc147223b3660cb739
Aarav Navani <aarav.navani@outlook.com>
2021-06-24 15:45:54 +0530

    Added GetCurrentUserAsync method

--- MystPaste.NET/Clients/UserClient.cs
+++ MystPaste.NET/Clients/UserClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using MystPaste.NET.Helpers;
 using MystPaste.NET.Models;
 
@@ -33,6 +34,23 @@ namespace MystPaste.NET.Clients
         public async Task<User> GetUserAsync(string username)
         {
             return await ApiRequester.Get<User>(ApiUrls.User(username));
+        }
+
+        /// <summary>
+        /// Gets the currently authenticated user.
+        /// </summary>
+        /// <param name="auth">
+        /// An optional authentication token.
+        /// Is only optional when a token has not been passed to the <see cref="MystPasteClient"/>.</param>
+        /// <returns>A <see cref="CurrentUser"/> object.</returns>
+        /// <exception cref="ArgumentNullException">Throws when an auth token has not been passed to the client or the method.</exception>
+        public async Task<CurrentUser> GetCurrentUserAsync(string auth = null)
+        {
+            auth ??= ApiRequester.Auth;
+            if (auth is null)
+                throw new ArgumentNullException(nameof(auth), "An authorization token needs to be passed in the constructor of the MystPasteClient or to the method");
+
+            return await ApiRequester.Get<CurrentUser>(ApiUrls.CurrentUser(), auth);
         }
     }
 }
