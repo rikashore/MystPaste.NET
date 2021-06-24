08e9d434a15dd1bfc6bb70108fe7159288310720
Aarav Navani <aarav.navani@outlook.com>
2021-06-24 14:07:01 +0530

    Documented models

--- MystPaste.NET/Models/Language.cs
+++ MystPaste.NET/Models/Language.cs
@@ -1,26 +1,56 @@
 ﻿using System.Collections.Generic;
+using System.Drawing;
+using MystPaste.NET.Extensions;
 using Newtonsoft.Json;
 
 namespace MystPaste.NET.Models
 {
+    /// <summary>
+    /// Represents a model to map the response for.
+    /// the language data endpoints
+    /// </summary>
     public class Language
     {
+        /// <summary>
+        /// An optional list of strings with the extensions used by the language.
+        /// </summary>
         [JsonProperty("ext")]
         public List<string> Extensions { get; set; }
         
+        /// <summary>
+        /// An optional list of strings with the aliases of the language.
+        /// </summary>
         [JsonProperty("alias")]
         public List<string> Aliases { get; set; }
         
+        /// <summary>
+        /// The name of the language.
+        /// </summary>
         [JsonProperty("name")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// The color used to represent the language. 
+        /// </summary>
         [JsonProperty("color")]
-        public string Color { get; set; }
+        public string HexColor { get; set; }
 
+        /// <summary>
+        /// The language mode to be used in the editor.
+        /// </summary>
         [JsonProperty("mode")]
         public string Mode { get; set; }
 
+        /// <summary>
+        /// A list of mimes used by the language.
+        /// </summary>
         [JsonProperty("mimes")]
         public List<string> Mimes { get; set; }
+
+        /// <summary>
+        /// A <see cref="System.Drawing.Color"/> formed from HexColor.
+        /// </summary>
+        [JsonIgnore]
+        public Color Color => HexColor.ParseColor();
     }
 }
--- MystPaste.NET/Models/User.cs
+++ MystPaste.NET/Models/User.cs
@@ -2,26 +2,50 @@
 
 namespace MystPaste.NET.Models
 {
+    /// <summary>
+    /// Represents a Paste Myst User.
+    /// </summary>
     public class User
     {
+        /// <summary>
+        /// The Id of the user.
+        /// </summary>
         [JsonProperty("_id")]
         public string Id { get; set; }
         
+        /// <summary>
+        /// The user's username.
+        /// </summary>
         [JsonProperty("username")]
         public string Username { get; set; }
         
+        /// <summary>
+        /// The user's Avatar URL.
+        /// </summary>
         [JsonProperty("avatarUrl")]
         public string AvatarUrl { get; set; }
         
+        /// <summary>
+        /// The default language of the user.
+        /// </summary>
         [JsonProperty("defaultLang")]
         public string DefaultLanguage { get; set; }
         
+        /// <summary>
+        /// Whether a user has a public profile or not.
+        /// </summary>
         [JsonProperty("publicProfile")]
         public bool HasPublicProfile { get; set; }
         
+        /// <summary>
+        /// How long the user has been a supporter. 0 if not a supporter.
+        /// </summary>
         [JsonProperty("supporterLength")]
         public uint SupporterLength { get; set; }
         
+        /// <summary>
+        /// Whether a user has contributed to Paste Myst.
+        /// </summary>
         [JsonProperty("contributor")]
         public bool IsContributor { get; set; }
     }
