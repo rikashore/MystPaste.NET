40941693912ab89c0bb7682cf2e1bcaa799da089
Aarav Navani <aarav.navani@outlook.com>
2021-06-24 08:25:18 +0530

    Added example project

---
+++ MystPaste.NET.Example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MystPaste.NET.Example
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var myst = new MystPasteClient();
+
+            var res = await myst.Data.GetLanguageByNameAsync("C#");
+            
+            Console.WriteLine(res.Color);
+            
+            var ext = await myst.Data.GetLanguageByExtensionAsync(res.Extensions[0]);
+            
+            Console.WriteLine(ext.Name);
+        }
+    }
+}
