d78071a39c5cc1b6f25513015db67014f698548e
Aarav Navani <aarav.navani@outlook.com>
2021-06-24 17:17:04 +0530

    Added paste client

---
+++ MystPaste.NET/Clients/PasteClient.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using MystPaste.NET.Helpers;
+using MystPaste.NET.Models;
+
+namespace MystPaste.NET.Clients
+{
+    public class PasteClient : ApiClient
+    {
+        public PasteClient(ApiRequester apiRequester) : base(apiRequester)
+        { }
+
+        // TODO Fix broken Json
+        public async Task<Paste> GetPasteAsync(string pasteId)
+        {
+            return await ApiRequester.Get<Paste>(ApiUrls.Paste(pasteId));
+        }
+    }
+}
