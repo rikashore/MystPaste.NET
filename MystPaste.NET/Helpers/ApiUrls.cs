7fcb808e8277fcb701138fa3e5e91766a5fcc9a5
Aarav Navani <aarav.navani@outlook.com>
2021-06-24 08:24:50 +0530

    Added a few user endpoints

--- MystPaste.NET/Helpers/ApiUrls.cs
+++ MystPaste.NET/Helpers/ApiUrls.cs
@@ -9,6 +9,12 @@ namespace MystPaste.NET.Helpers
             => "data/language?name={0}".FormatUri(language);
 
         public static Uri LanguageByExtension(string extension)
-            => "/data/languageExt?extension={0}".FormatUri(extension);
+            => "data/languageExt?extension={0}".FormatUri(extension);
+
+        public static Uri UserExists(string username)
+            => "user/{0}/exists".FormatUri(username);
+
+        public static Uri User(string username)
+            => "user/{0}".FormatUri(username);
     }
 }
