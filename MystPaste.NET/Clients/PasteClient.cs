9f533d03d297f0f2abc4d9910f68819950d5804f
Aarav Navani <aarav.navani@outlook.com>
2021-06-25 10:13:33 +0530

    Documented methods

--- MystPaste.NET/Clients/PasteClient.cs
+++ MystPaste.NET/Clients/PasteClient.cs
@@ -10,12 +10,29 @@ namespace MystPaste.NET.Clients
         public PasteClient(ApiRequester apiRequester) : base(apiRequester)
         { }
         
+        /// <summary>
+        /// Gets a paste by its id.
+        /// <seealso cref="GetAuthenticatedPasteAsync(string, string)"/>
+        /// </summary>
+        /// <param name="pasteId">The id of the paste to get.</param>
+        /// <returns>A <see cref="Paste"/> object.</returns>
         public Task<Paste> GetPasteAsync(string pasteId)
         {
             return ApiRequester.Get<Paste>(ApiUrls.Paste(pasteId));
         }
 
-        public Task<Paste> GetPasteAsync(string pasteId, string auth)
+        /// <summary>
+        /// Get a private paste via id. requires auth.
+        /// <seealso cref="GetPasteAsync"/>
+        /// </summary>
+        /// <param name="pasteId">The id of the paste to get.</param>
+        /// <param name="auth">
+        /// An optional authentication token.
+        /// Is only optional when a token has not been passed to the <see cref="MystPasteClient"/>.
+        /// </param>
+        /// <returns>A <see cref="Paste"/> object.</returns>
+        /// <exception cref="InvalidAuthException">Throws when an auth token has not been passed to the client or the method.</exception>
+        public Task<Paste> GetAuthenticatedPasteAsync(string pasteId, string auth = null)
         {
             auth ??= ApiRequester.Auth;
             if (auth is null)
