f0e296b87263a47c6c7aa9630dd13c72bca7f072
Aarav Navani <aarav.navani@outlook.com>
2021-06-25 16:52:17 +0530

    Added post and delete methods

--- MystPaste.NET/ApiRequester.cs
+++ MystPaste.NET/ApiRequester.cs
@@ -58,5 +58,43 @@ namespace MystPaste.NET
             var res = await _httpClient.GetAsync(uri);
             return res.IsSuccessStatusCode;
         }
+
+        public async Task Post(Uri uri, string content, string auth = null)
+        {
+            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, uri);
+
+            if (auth is not null)
+                requestMessage.Headers.TryAddWithoutValidation("Authorization", auth);
+
+            requestMessage.Content = new StringContent(content);
+
+            var res = await _httpClient.SendAsync(requestMessage);
+            var s = await res.Content.ReadAsStreamAsync();
+
+            if (!res.IsSuccessStatusCode)
+            {
+                var err = s.DeserializeTo<Response>();
+                throw new Exception(err is null
+                    ? "The server returned an exception with unknown reasons."
+                    : $"The server returned an exception: {err.ErrorMessage}");
+            }
+        }
+
+        public async Task Delete(Uri uri, string auth)
+        {
+            using var requestMessage = new HttpRequestMessage(HttpMethod.Delete, uri);
+            requestMessage.Headers.TryAddWithoutValidation("Authorization", auth);
+            
+            var res = await _httpClient.SendAsync(requestMessage);
+            var s = await res.Content.ReadAsStreamAsync();
+
+            if (!res.IsSuccessStatusCode)
+            {
+                var err = s.DeserializeTo<Response>();
+                throw new Exception(err is null
+                    ? "The server returned an exception with unknown reasons."
+                    : $"The server returned an exception: {err.ErrorMessage}");
+            }
+        }
     }
 }
